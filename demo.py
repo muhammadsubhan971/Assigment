4ab9ab3ef4723bc688a38fce53c628dfe7de7478
muhammadsubhan971 <subhan.kh753@gmail.com>
2024-08-01 09:30:01 -0700

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,12 @@
+# ► My name is Muhammad_Subhan.
+# ► I am 19 years old.
+# ► I compeleted my matric from technical school.
+# ► Then i am compeleted my intermediate in ICS form superior colleges.
+# ► Now i'm currently in 3rd semester of BS Artificial Intelligence.
+# ► It's a great pleasure for to learn gentrative Artificial Intelligence from honorable organization.
+# ► From the beginging i was always interested in AI and how it can be used in real life.
+# ► It's my desire to become a data science professional so this course will help me to become a data scientist.
+# ► And my short term goals are that i put my 100% effort in this course.
+# ► And it's my desire to serve PIAIC in the future.
+    
+#                         ________________
